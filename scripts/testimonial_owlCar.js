6f47075170464a6d58dadf29c30413b8ea25056d
abdul-malik360 <82204978+abdul-malik360@users.noreply.github.com>
2021-08-21 10:10:25 +0200

    update

---
+++ scripts/testimonial_owlCar.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      items: 2,
+      margin: 10,
+      loop: true,
+      stagePadding: 50,
+      dots: false,
+      autoplay: true,
+    });
+  });
